6f96ccdb23bbdd54eccbb0099e164242e87066bb
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-05-19 11:43:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Belajar Membuat Front-End Web untuk Pemula
+# web-pemula
+Submission (proyek akhir) membuat sebuah website untuk mencatat penyimpanan buku dengan menerapkan teknik manipulasi DOM dan web storage
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { addBookButton, search, simpanBuku, SAVED_BOOK, LOAD_BOOK, EVENT_CUSTOM } from "./scripts/constants.js";
+import {
+  inputBookHandler,
+  searchButtonHandler,
+  keyUpHandler,
+  submitBookHandler,
+  closeForm,
+  refreshed,
+  showFormInput,
+  evenCustomHandlers
+} from "./scripts/functions.js";
+
+
+simpanBuku.addEventListener("submit", inputBookHandler);
+search.addEventListener("keyup", keyUpHandler);
+search.addEventListener("submit", searchButtonHandler);
+document.addEventListener("DOMContentLoaded", submitBookHandler);
+document.addEventListener(SAVED_BOOK, closeForm);
+document.addEventListener(LOAD_BOOK, refreshed);
+addBookButton.addEventListener("click", showFormInput);
+document.addEventListener(EVENT_CUSTOM, evenCustomHandlers);
